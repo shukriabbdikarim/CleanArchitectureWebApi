7395add9291eddb56eeff171731002e4a14a2444
shukriabbdikarim <shukri098@hotmail.com>
2026-05-27 23:32:55 +0200

    Update Program configuration

--- API/Program.cs
+++ API/Program.cs
@@ -9,7 +9,7 @@ namespace API
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {   // Swagger configuration
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services
